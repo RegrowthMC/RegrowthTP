c9a356286b18e8614cf1307e76bf30e726bf03ed
Oak <55620516+OakLoaf@users.noreply.github.com>
2026-02-26 19:11:36 +0000

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,67 @@
+plugins {
+    `java-library`
+    id("com.gradleup.shadow") version("9.3.1")
+    id("xyz.jpenilla.run-paper") version("3.0.2")
+}
+
+group = "org.lushplugins"
+version = "1.0.0"
+
+repositories {
+    mavenLocal()
+    mavenCentral()
+    maven("https://oss.sonatype.org/content/groups/public/")
+    maven("https://repo.papermc.io/repository/maven-public/") // Paper
+}
+
+dependencies {
+    // Dependencies
+    compileOnly("io.papermc.paper:paper-api:1.21.7-R0.1-SNAPSHOT")
+
+    // Soft Dependencies
+
+    // Libraries
+}
+
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(21))
+
+    registerFeature("optional") {
+        usingSourceSet(sourceSets["main"])
+    }
+
+    withSourcesJar()
+}
+
+tasks {
+    withType<JavaCompile> {
+        options.encoding = "UTF-8"
+        options.compilerArgs.add("-parameters")
+    }
+
+    shadowJar {
+        minimize()
+
+        archiveFileName.set("${project.name}-${project.version}.jar")
+    }
+
+    processResources{
+        filesMatching("plugin.yml") {
+            expand(project.properties)
+        }
+
+        inputs.property("version", rootProject.version)
+        filesMatching("plugin.yml") {
+            expand("version" to rootProject.version)
+        }
+    }
+
+    runServer {
+        minecraftVersion("1.21.11")
+
+        downloadPlugins {
+            modrinth("viaversion", "5.7.1")
+            modrinth("viabackwards", "5.7.1")
+        }
+    }
+}
